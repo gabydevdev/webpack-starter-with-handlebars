95f2468a7720aac359f2c23f07b79a2534b5bf47
Gaby F. <1626284+iamgaby7521@users.noreply.github.com>
2024-04-20 12:48:34 -0700

    🧱 improve structure for handlebar templating

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,1 @@
-import "../styles/index.scss";
-
-console.log("webpack starterkit");
-
-async function init() {
-	// import all your functions/code there
-}
-
-if (document.readyState === "complete") {
-	init();
-} else {
-	window.addEventListener("load", init);
-}
-
-console.log(process.env.NODE_ENV);
+console.log(">> main");
